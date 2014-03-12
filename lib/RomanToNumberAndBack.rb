8c09e777130922b5229395312e61e45d0257d2ab
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2014-03-12 00:53:21 -0400

    final

--- lib/RomanToNumberAndBack.rb
+++ lib/RomanToNumberAndBack.rb
@@ -5,12 +5,42 @@ def toAppropriateValue(input)
   return to_number(input)
 end
 
+MapRomansAndNumbers = {
+  'M' => 1000,
+  'CM' => 900,
+  'D' => 500,
+  'CD' => 400,
+  'C' => 100,
+  'XC' => 90,
+'L' => 50,
+'XL' => 40,
+'X' => 10,
+'IX' => 9,
+'V' => 5,
+'IV' => 4,
+'I' => 1
+}
+
 def to_roman(num)
-  return 'I'*num
+  result = ''
+  MapRomansAndNumbers.each do |roman, number|
+  while num >= number do
+    result += roman
+    num -= number
+  end
+end
+  return result
 end
 
 def to_number(roman)
-  return 1
+   result = 0
+     roman.each_char do |roman_char|
+     result += MapRomansAndNumbers[roman_char]
+   end
+   MapRomansAndNumbers.each do |key , value|
+    result -= 2 * MapRomansAndNumbers[key[0]] if roman.include? key and key.length == 2
+   end
+ return result
 end
 
 end
--- spec/lib/RomanToNumberAndBack_spec.rb
+++ spec/lib/RomanToNumberAndBack_spec.rb
@@ -8,5 +8,37 @@ specify { convert.toAppropriateValue('I').should == 1 }
 specify { convert.toAppropriateValue(1).should == 'I' }
 specify { convert.to_roman(2).should == 'II' }
 specify { convert.to_roman(3).should == 'III' }
+specify { convert.to_roman(4).should == 'IV'}
+specify { convert.to_roman(5).should == 'V'}
+specify { convert.to_roman(6).should == 'VI'}
+specify { convert.to_roman(7).should == 'VII'}
+specify { convert.to_roman(9).should == 'IX'}
+specify { convert.to_roman(10).should == 'X'}
+specify { convert.to_roman(14).should == 'XIV'}
+specify { convert.to_roman(40).should == 'XL'}
+specify { convert.to_roman(50).should == 'L'}
+specify { convert.to_roman(90).should == 'XC'}
+specify { convert.to_roman(100).should == 'C'}
+specify { convert.to_roman(400).should == 'CD'}
+specify { convert.to_roman(500).should == 'D'}
+specify { convert.to_roman(900).should == 'CM'}
+specify { convert.to_roman(1000).should == 'M'}
 
+specify { convert.to_number('II').should == 2 }
+specify { convert.to_number('III').should == 3 }
+specify { convert.to_number('V').should == 5 }
+specify { convert.to_number('VI').should == 6 }
+specify { convert.to_number('X').should == 10 }
+specify { convert.to_number('L').should == 50 }
+specify { convert.to_number('C').should == 100 }
+specify { convert.to_number('D').should == 500 }
+specify { convert.to_number('M').should == 1000 }
+specify { convert.to_number('MMDCLXVI').should == 2666 }
+specify { convert.to_number('IV').should == 4 }
+specify { convert.to_number('IX').should == 9 }
+specify { convert.to_number('XL').should == 40 }
+specify { convert.to_number('XC').should == 90 }
+specify { convert.to_number('CD').should == 400 }
+specify { convert.to_number('CM').should == 900 }
+specify { convert.to_number('MMMCMXCIX').should == 3999 }
 end
